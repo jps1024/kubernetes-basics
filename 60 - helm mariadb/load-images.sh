c95ebfeac5e05d3dd61a0ea4da396ecb52f1125d
Joao Pedro Sousa <jps1024@gmail.com>
2025-12-20 22:19:58 +0000

    Introducing the values 'mini' version and fixing the Prometheus version

--- 60 - helm mariadb/load-images.sh
+++ 60 - helm mariadb/load-images.sh
@@ -5,8 +5,8 @@ CLUSTERNAME=k2
 docker pull              docker.io/bitnamilegacy/mariadb:12.0.2-debian-12-r0
 kind   load docker-image docker.io/bitnamilegacy/mariadb:12.0.2-debian-12-r0          --name ${CLUSTERNAME}
 
-docker pull              docker.io/bitnamilegacy/os-shell:12-debian-12-r50
-kind   load docker-image docker.io/bitnamilegacy/os-shell:12-debian-12-r50            --name ${CLUSTERNAME}
-
 docker pull              docker.io/bitnamilegacy/mysqld-exporter:0.17.2-debian-12-r16
 kind   load docker-image docker.io/bitnamilegacy/mysqld-exporter:0.17.2-debian-12-r16 --name ${CLUSTERNAME}
+
+docker pull              docker.io/alpine/curl:3.14
+kind   load docker-image docker.io/alpine/curl:3.14                                   --name ${CLUSTERNAME}
